0bef25be45d2c844808e30960972eadb1f4ded99
Felipe Restrepo <maferelo13@gmail.com>
2018-04-26 14:47:07 -0500

    Create solver view

--- saleor/landing/views.py
+++ saleor/landing/views.py
@@ -1,7 +1,13 @@
-from django.template.response import TemplateResponse
-from django.shortcuts import get_object_or_404, redirect
+import logging
 
-from .forms import CityCreateForm, EncuestaForm
+from django.template.response import TemplateResponse
+from django.shortcuts import redirect
+
+from .forms import EncuestaForm
+from .utils import get_routes
+from .models import Encuesta, Routes
+
+logger = logging.getLogger(__name__)
 
 
 def landing_home(request):
@@ -13,8 +19,21 @@ def landing_home(request):
 def landing_encuesta(request):
     form = EncuestaForm(request.POST or None)
     if form.is_valid():
+        form.save()
         return redirect('landing_urls:landing_home')
     return TemplateResponse(
         request, 'landing/landing_encuesta.html',
         {'parent': None,
          'form': form})
+
+
+def landing_solver(request):
+    customers_q = Encuesta.objects.all()
+    logger.info(len(customers_q))
+    routes = get_routes(customers_q)
+    return TemplateResponse(
+        request, 'landing/landing_solver.html',
+        {'parent': None,
+         'routes': routes})
+
+
