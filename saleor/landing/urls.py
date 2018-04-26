d262936567bedc8aa4f189fa08ff6d787de917f9
Felipe Restrepo <maferelo13@gmail.com>
2018-04-26 14:46:13 -0500

    Add solver url

--- saleor/landing/urls.py
+++ saleor/landing/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^encuesta/$', views.landing_encuesta, name='landing_encuesta'),
+    url(r'^optimize/$', views.landing_solver, name='landing_solver'),
     url(r'^$', views.landing_home, name='landing_home')
 ]
